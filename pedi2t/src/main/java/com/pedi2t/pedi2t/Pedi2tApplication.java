f0721ec409896cad7a512aab5b4ba913b6f22ba8
--get <--get>
2025-10-28 18:02:10 -0300

    Inicializacion del proyecto

---
+++ pedi2t/src/main/java/com/pedi2t/pedi2t/Pedi2tApplication.java
@@ -0,0 +1,13 @@
+package com.pedi2t.pedi2t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedi2tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pedi2tApplication.class, args);
+	}
+
+}
---
+++ pedi2t/src/test/java/com/pedi2t/pedi2t/Pedi2tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedi2t.pedi2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedi2tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
