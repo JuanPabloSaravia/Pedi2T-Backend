2d3b61d5c7ab3c1d8a8482a651f31b465ab4c8fb
--get <--get>
2025-11-05 18:57:43 -0300

    Home Controller v2

--- pedi2t/src/main/java/com/pedi2t/pedi2t/DTO/LoginResponseDTO.java
+++ pedi2t/src/main/java/com/pedi2t/pedi2t/DTO/LoginResponseDTO.java
@@ -1,8 +1,10 @@
 package com.pedi2t.pedi2t.DTO; // (Usa tu paquete)
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class LoginResponseDTO {
     
     private String token;
